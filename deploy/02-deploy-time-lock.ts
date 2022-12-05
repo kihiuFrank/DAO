b73e7a0b97c523be6d6c0076c10ba85a1ea1efdb
kihiuFrank <franklinekihiu@gmail.com>
2022-12-05 11:07:56 +0300

    deploying timeLock

---
+++ deploy/02-deploy-time-lock.ts
@@ -0,0 +1,21 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/dist/types"
+import { parseArgs } from "util"
+import { networkConfig, MIN_DELAY } from "../helper-hardhat-config"
+import { network } from "hardhat"
+
+const deployTimeLock: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { getNamedAccounts, deployments } = hre
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    log("Deploying TimeLock...")
+    const args = [MIN_DELAY, [], [], deployer]
+    const timeLock = await deploy("TimeLock", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
+    })
+}
+
+export default deployTimeLock
