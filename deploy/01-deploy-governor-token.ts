c3550dad8853bac4b682f7681fecbfefa9a84ed8
kihiuFrank <franklinekihiu@gmail.com>
2022-12-05 10:14:54 +0300

    contract verification

--- deploy/01-deploy-governor-token.ts
+++ deploy/01-deploy-governor-token.ts
@@ -1,5 +1,7 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/dist/types"
+import { networkConfig, developmentChains } from "../helper-hardhat-config"
+import verify from "../helper-functions"
 
 const deployGovernanceToken: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     const { getNamedAccounts, deployments, network } = hre
@@ -10,8 +12,14 @@ const deployGovernanceToken: DeployFunction = async (hre: HardhatRuntimeEnvironm
         from: deployer,
         args: [],
         log: true,
+        // we need to wait if on a live network so we can verify properly
+        waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
     })
     log(`Deployed governance token to address ${governanceToken.address}`)
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        await verify(governanceToken.address, [])
+    }
 }
 
 export default deployGovernanceToken
