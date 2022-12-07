97e20b9fb8e85df27972212dbfa9721471242657
kihiuFrank <franklinekihiu@gmail.com>
2022-12-07 12:35:27 +0300

    Governance Flow Unit Tests

--- test/unit/testflow.ts
+++ test/unit/testflow.ts
@@ -18,7 +18,7 @@ describe("Governor Flow", async () => {
     let timeLock: TimeLock
     let box: Box
     const voteWay = 1 // for
-    const reason = "I lika do da cha cha"
+    const reason = "Open source is the way to go "
     beforeEach(async () => {
         await deployments.fixture(["all"])
         governor = await ethers.getContract("GovernorContract")
@@ -29,5 +29,56 @@ describe("Governor Flow", async () => {
 
     it("can only be changed through governance", async () => {
         await expect(box.store(55)).to.be.revertedWith("Ownable: caller is not the owner")
+    })
+
+    it("proposes, votes, waits, queues, and then executes", async () => {
+        // propose
+        const encodedFunctionCall = box.interface.encodeFunctionData(FUNC, [NEW_STORE_VALUE])
+        const proposeTx = await governor.propose(
+            [box.address],
+            [0],
+            [encodedFunctionCall],
+            PROPOSAL_DESCRIPTION
+        )
+
+        const proposeReceipt = await proposeTx.wait(1)
+        const proposalId = proposeReceipt.events![0].args!.proposalId
+        let proposalState = await governor.state(proposalId)
+        console.log(`Current Proposal State: ${proposalState}`)
+
+        await moveBlocks(VOTING_DELAY + 1)
+        // vote
+        const voteTx = await governor.castVoteWithReason(proposalId, voteWay, reason)
+        await voteTx.wait(1)
+        proposalState = await governor.state(proposalId)
+        assert.equal(proposalState.toString(), "1")
+        console.log(`Current Proposal State: ${proposalState}`)
+        await moveBlocks(VOTING_PERIOD + 1)
+
+        // queue & execute
+        // const descriptionHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(PROPOSAL_DESCRIPTION))
+        const descriptionHash = ethers.utils.id(PROPOSAL_DESCRIPTION)
+        const queueTx = await governor.queue(
+            [box.address],
+            [0],
+            [encodedFunctionCall],
+            descriptionHash
+        )
+        await queueTx.wait(1)
+        await moveTime(MIN_DELAY + 1)
+        await moveBlocks(1)
+
+        proposalState = await governor.state(proposalId)
+        console.log(`Current Proposal State: ${proposalState}`)
+
+        console.log("Executing...")
+        const executeTx = await governor.execute(
+            [box.address],
+            [0],
+            [encodedFunctionCall],
+            descriptionHash
+        )
+        await executeTx.wait(1)
+        console.log((await box.retrieve()).toString())
     })
 })
